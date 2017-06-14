952bb6a8f7bdd28b9fa50a5446be21e428589262
ak-sahli <ak-sahli@protonmail.com>
2017-06-15 00:14:16 +0200

    Implementation for deposit and withdrawal of an amount of money

--- src/test/kotlin/io/aksahli/kata/bank/AccountUnitTest.kt
+++ src/test/kotlin/io/aksahli/kata/bank/AccountUnitTest.kt
@@ -1,25 +1,71 @@
 package io.aksahli.kata.bank
 
-import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.subject.SubjectSpek
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
-class AccountUnitTest : Spek({
+object AccountUnitTest : SubjectSpek<Account>({
 
     describe("a bank account") {
 
-        it("should increase the balance after a deposit of a valid amount of money") {
+        subject { Account(owner = "Bruce Wayne", initialAmount = 1000.00) }
 
+        on("deposit") {
+            it("should increase the balance after a deposit of a valid amount of money") {
+                subject deposit 500.00
+                assertEquals(1500.00, subject.balance())
+            }
+            it("should throw an error after a withdrawal of an invalid amount of money") {
+                assertFailsWith(IllegalAmountException::class) {
+                    subject deposit -500.00
+                }
+            }
         }
 
-        it("should decrease the balance after a withdrawal of a valid amount of money") {
-
-        }
-
-        it("should keep the balance as it is after a withdrawal of an invalid amount of money") {
-
+        on("withdraw") {
+            it("should decrease the balance after a withdrawal of a valid amount of money") {
+                subject withdraw 500.00
+                assertEquals(500.00, subject.balance())
+            }
+            it("should throw an error after a withdrawal of an invalid amount of money") {
+                assertFailsWith(IllegalAmountException::class) {
+                    subject withdraw  -500.00
+                }
+            }
         }
 
     }
 
 })
+
+typealias Amount = Double
+
+data class IllegalAmountException(val amount: Amount)
+    : IllegalArgumentException("Invalid requested amount: $amount")
+
+data class Account(val owner: String, val initialAmount: Amount) {
+
+    private var balance = initialAmount
+
+    private fun validateAmount(amount: Amount) {
+        if (amount < 0) {
+            throw IllegalAmountException(amount)
+        }
+    }
+
+    infix fun deposit(amount: Amount) {
+        validateAmount(amount)
+        this.balance = this.balance + amount
+    }
+
+    infix fun  withdraw(amount: Amount) {
+        validateAmount(amount)
+        this.balance = this.balance - amount
+    }
+
+    fun balance(): Amount = this.balance
+
+}
