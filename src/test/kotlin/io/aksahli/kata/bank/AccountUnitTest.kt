7594819ef935b51ad46c894c154c423350b448fe
ak-sahli <ak-sahli@protonmail.com>
2017-06-14 23:19:23 +0200

    Unit tests for deposit and withdrawal of a money amount

---
+++ src/test/kotlin/io/aksahli/kata/bank/AccountUnitTest.kt
@@ -0,0 +1,25 @@
+package io.aksahli.kata.bank
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class AccountUnitTest : Spek({
+
+    describe("a bank account") {
+
+        it("should increase the balance after a deposit of a valid amount of money") {
+
+        }
+
+        it("should decrease the balance after a withdrawal of a valid amount of money") {
+
+        }
+
+        it("should keep the balance as it is after a withdrawal of an invalid amount of money") {
+
+        }
+
+    }
+
+})
